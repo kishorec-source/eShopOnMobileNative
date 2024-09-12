a075bba4f9e92a061aafac6e7526a244c6cca338
kchandra <kishore.chandra@learningmate.com>
2024-09-12 19:44:27 +0530

    removed unused

--- src/routes/root.route.tsx
+++ src/routes/root.route.tsx
@@ -1,9 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import { CartPage, HomePage } from "../views";
-import CardLayers3d from "../components/details/cards.component";
-import Ck4 from "../components/details/editor.component";
 import ErrorPage from "../components/ErrorPage";
-import TickTackToe from "../components/details/TickTackToe";
 
 export const router = createBrowserRouter([
   {
@@ -13,14 +10,6 @@ export const router = createBrowserRouter([
   {
     path: "/cart",
     element: <CartPage />,
-  },
-  {
-    path: "/home",
-    element: <CardLayers3d />,
-  },
-  {
-    path: "/ck4",
-    element: <Ck4 />,
   },
   {
     path: "/404",
